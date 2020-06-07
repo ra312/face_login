575a23a116255f5c70d29819460e09277d62aa6b
ra312 <akylzhanov.r@gmail.com>
2020-06-07 14:10:28 +0600

    changed to minimal working version

--- face_login/face_login_api/api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url
-from .views import NewImage
-
-urlpatterns = [
-    url(r'^image/$', NewImage.as_view(), name='upload-image'),
-]
--- two_factor_face/mysite/mysite/wsgi.py
+++ two_factor_face/mysite/mysite/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for face_login_api project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'face_login_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
